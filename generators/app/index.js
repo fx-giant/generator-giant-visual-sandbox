6ec08151caa640dff475541f368a50b8379020d4
Hans Yulian <hansyulian@windowslive.com>
2017-11-29 14:07:03 +0800

    convert indentation to 4 space

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,64 +5,64 @@ const yosay = require('yosay');
 const visualPackFolder = "source";
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the cat\'s meow ' + chalk.red('generator-giant-visual-sandbox') + ' generator!'
-    ));
+    prompting() {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the cat\'s meow ' + chalk.red('generator-giant-visual-sandbox') + ' generator!'
+        ));
 
-    const prompts = [{
+        const prompts = [{
 
-      name: 'visualPackName',
-      message: 'Give a name to your visual pack (camel case)',
-      default: 'sampleVisualPack'
-    }];
+            name: 'visualPackName',
+            message: 'Give a name to your visual pack (camel case)',
+            default: 'sampleVisualPack'
+        }];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
-  writing() {
-    var justCopy = [
-      'package.json',
-      'dependencies',
-      'packs/.gitkeep',
-    ];
-    var copyWithRender = [
-      'index.html',
-      'packer.js',
-      'config.json',
-      'source/config.json',
-      'source/{{visualName}}.css',
-      'source/{{visualName}}.js',
-      'source/quadrant-properties-{{visualName}}.html',
-      'source/quadrant-properties-{{visualName}}.js'
-    ];
-
-    var visualName = this.props.visualPackName;
-    var templateValues = {
-      visualName: visualName
-    };
-    for (var i = 0; i < justCopy.length; i++) {
-      var file = justCopy[i];
-      this.fs.copy(
-        this.templatePath(file),
-        this.destinationPath(visualName + "/" + file)
-      )
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        });
     }
-    for (var i = 0; i < copyWithRender.length; i++) {
-      var file = copyWithRender[i];
-      this.fs.copyTpl(
-        this.templatePath(file),
-        this.destinationPath(visualName + '/' + file).replace(new RegExp('{{visualName}}', 'g'), visualName),
-        templateValues
-      );
+
+    writing() {
+        var justCopy = [
+            'package.json',
+            'dependencies',
+            'packs/.gitkeep',
+        ];
+        var copyWithRender = [
+            'index.html',
+            'packer.js',
+            'config.json',
+            'source/config.json',
+            'source/{{visualName}}.css',
+            'source/{{visualName}}.js',
+            'source/quadrant-properties-{{visualName}}.html',
+            'source/quadrant-properties-{{visualName}}.js'
+        ];
+
+        var visualName = this.props.visualPackName;
+        var templateValues = {
+            visualName: visualName
+        };
+        for (var i = 0; i < justCopy.length; i++) {
+            var file = justCopy[i];
+            this.fs.copy(
+                this.templatePath(file),
+                this.destinationPath(visualName + "/" + file)
+            )
+        }
+        for (var i = 0; i < copyWithRender.length; i++) {
+            var file = copyWithRender[i];
+            this.fs.copyTpl(
+                this.templatePath(file),
+                this.destinationPath(visualName + '/' + file).replace(new RegExp('{{visualName}}', 'g'), visualName),
+                templateValues
+            );
+        }
     }
-  }
 
-  install() {
+    install() {
 
-  }
+    }
 };
--- generators/app/templates/packer.js
+++ generators/app/templates/packer.js
@@ -13,7 +13,7 @@ var processType = argv[2] || "upload";
 const defaultSaveDirectory = "packs";
 
 var targets = {
-	SIT: "http://172.16.53.228/WebSites/Analytics/api/VisualPack/Upload",
+    SIT: "http://172.16.53.228/WebSites/Analytics/api/VisualPack/Upload",
 }
 
 var zipName = "<%= visualName %>";
@@ -28,87 +28,87 @@ var now = dateTime.create().format("Y-m-d_H-M-S");
 
 
 if (processType == "upload")
-	startUpload();
+    startUpload();
 else
 if (processType == "save")
-	startSave();
+    startSave();
 
 function startUpload() {
-	zipTheVisualPack(uploadFile);
+    zipTheVisualPack(uploadFile);
 }
 
 function startSave() {
-	zipTheVisualPack(savePack);
+    zipTheVisualPack(savePack);
 }
 
 function zipTheVisualPack(callback) {
-	var archive = archiver("zip", {
-		zlib: {
-			level: 9
-		}
-	})
-	var output = fs.createWriteStream(zipFile);
-	output.on("close", function () {
-		console.log("Files zipped to " + zipFile);
-		callback();
-	});
+    var archive = archiver("zip", {
+        zlib: {
+            level: 9
+        }
+    })
+    var output = fs.createWriteStream(zipFile);
+    output.on("close", function () {
+        console.log("Files zipped to " + zipFile);
+        callback();
+    });
 
-	fs.readdirSync(sourceDirectory).forEach(function (file) {
-		var read = fs.createReadStream(sourceDirectory + "/" + file);
-		archive.append(read, {
-			name: file
-		});
-	})
-	archive.pipe(output);
-	archive.finalize();
+    fs.readdirSync(sourceDirectory).forEach(function (file) {
+        var read = fs.createReadStream(sourceDirectory + "/" + file);
+        archive.append(read, {
+            name: file
+        });
+    })
+    archive.pipe(output);
+    archive.finalize();
 
 }
 
 function uploadFile() {
-	if (!cookie)
-		throw "Null cookie";
-	console.log("Upload File: ", zipFile);
-	console.log("Uploading to ", uploadUrl)
-	var requestObj = {
-		method: "post",
-		uri: uploadUrl,
-		headers: {
-			"Cookie": cookie
-		},
-		formData: {
-			attachments: [fs.createReadStream(zipFile)]
-		}
-	};
+    if (!cookie)
+        throw "Null cookie";
+    console.log("Upload File: ", zipFile);
+    console.log("Uploading to ", uploadUrl)
+    var requestObj = {
+        method: "post",
+        uri: uploadUrl,
+        headers: {
+            "Cookie": cookie
+        },
+        formData: {
+            attachments: [fs.createReadStream(zipFile)]
+        }
+    };
 
-	var req = request(requestObj, function (err, resp, body) {
-		try {
-			JSON.parse(body);
-			console.log("Upload Success");
-		} catch (ex) {
-			console.log("FAILED TO UPLOAD!");
-		}
-		finisher();
-	});
+    var req = request(requestObj, function (err, resp, body) {
+        try {
+            JSON.parse(body);
+            console.log("Upload Success");
+        } catch (ex) {
+            console.log("FAILED TO UPLOAD!");
+        }
+        finisher();
+    });
 }
 
 function finisher() {
-	fs.unlink(zipFile, function () {});
+    fs.unlink(zipFile, function () {});
 }
 
 function savePack() {
-	backupName = zipName + "__" + now + ".zip";
-	var backupTarget = saveDirectory + "/" + backupName;
-	console.log("Saving to " + backupTarget);
-	moveFile(zipFile, backupTarget);
+    backupName = zipName + "__" + now + ".zip";
+    var backupTarget = saveDirectory + "/" + backupName;
+    console.log("Saving to " + backupTarget);
+    moveFile(zipFile, backupTarget);
 }
 
 function moveFile(from, to, callback) {
-	var is = fs.createReadStream(from);
-	var os = fs.createWriteStream(to);
+    var is = fs.createReadStream(from);
+    var os = fs.createWriteStream(to);
 
-	is.pipe(os);
-	is.on('end', function () {
-		fs.unlink(from, function () {});
-		(callback || function () {})();
-	});
+    is.pipe(os);
+    is.on('end', function () {
+        fs.unlink(from, function () {});
+        (callback || function () {})();
+    });
 }
